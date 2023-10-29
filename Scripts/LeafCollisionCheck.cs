5e31ccef1e9b2c9823f3babc797af78beb215a10
Ilmari Vainio <ilmari.vainio01@gmail.com>
2023-10-29 12:24:56 +0200

    something

--- Scripts/LeafCollisionCheck.cs
+++ Scripts/LeafCollisionCheck.cs
@@ -3,17 +3,21 @@ using System;
 
 public partial class LeafCollisionCheck : Area2D
 {
-    [Export]
-    public int healthLeft = 10;
-    [Export]
-    public TextEdit healthleftText;
+	[Export]
+	private int healthLeft = 10;
+	[Export]
+	private TextEdit healthleftText;
 
+	[Export]
+	private Node wind;
+	[Export]
+	private Panel gameOver;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-        healthleftText.Text = healthLeft.ToString();
-    }
+		healthleftText.Text = healthLeft.ToString();
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
@@ -27,6 +31,10 @@ public partial class LeafCollisionCheck : Area2D
 			body.SetProcess(false);
 			healthLeft--;
 			healthleftText.Text= healthLeft.ToString();
+			if(healthLeft <= 0) {
+				wind.SetProcess(false);
+				gameOver.Visible= true;
+			}
 		}
 	}
 
